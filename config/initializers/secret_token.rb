c0a7f824206774dc8f1b36c3d2192ae351aacdb7
sasata299 <sasata299@livedoor.com>
2011-08-01 20:53:32 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanzaku::Application.config.secret_token = '065e4bdfb6ccac8d88dde204bb05e122aec547f1890760bc5529a9a8077d33eaceb3f9059356cb43588ff4b1b77d673963815a6bf8f4e46f5eeb0796caa441bf'

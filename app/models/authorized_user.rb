120fb71c0754d1c2f1785eb3421131feba4c1d63
sasata299 <sasata299@livedoor.com>
2011-08-16 06:59:14 +0900

    change user_name to use first_name and last_name

--- app/models/authorized_user.rb
+++ app/models/authorized_user.rb
@@ -2,7 +2,7 @@ class AuthorizedUser < ActiveRecord::Base
   def self.store(user)
     if !find_by_user_id(user["id"])
       create(
-        :user_name => user["username"],
+        :user_name => "#{user["last_name"]} #{user["first_name"]}",
         :user_id => user["id"],
         :email => user["email"]
       )

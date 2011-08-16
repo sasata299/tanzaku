01447436c4bd01161146d59f7cb68515e7f3bf1b
sasata299 <sasata299@livedoor.com>
2011-08-17 08:20:29 +0900

    character code UTF8 to ISO-2022-JP

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -5,7 +5,7 @@ module Mailer
       :secret_access_key => secret_access_key
     )
     ses.send_email(
-      :subject => "つながり検索からのお知らせです",
+      :subject => "つながり短冊からのお知らせです",
       :text_body => body(user, target_user),
       :source => from_email,
       :to => to_email
@@ -23,10 +23,14 @@ module Mailer
   end
 
   def from_email
-    ENV["FROM_EMAIL"]
+    %Q!"#{encode("つながり短冊")}" <#{ENV["FROM_EMAIL"]}>!
   end
 
   def body(user, target_user)
     "#{user["last_name"]} #{user["first_name"]}さんが#{target_user["last_name"]} #{target_user["first_name"]}さんと会いたいようです。"
+  end
+
+  def encode(subject)
+    NKF.nkf('-j -W --cp932', subject)
   end
 end

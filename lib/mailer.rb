8cdb597e805aabf2690deb6fc60f9c094eb6cfdc
sasata299 <sasata299@livedoor.com>
2011-09-05 01:16:34 +0900

    modify email message

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -5,7 +5,7 @@ module Mailer
       :secret_access_key => Tanzaku::Application.config.secret_access_key
     )
     ses.send_email(
-      :subject => "つながり短冊からのお知らせです",
+      :subject => "#{name(user)}さんの願い事が届きました！",
       :text_body => body(user, target_user),
       :source => from_email,
       :to => to_email
@@ -15,15 +15,21 @@ module Mailer
   private
 
   def from_email
-    %Q!"#{encode("つながり短冊")}" <#{Tanzaku::Application.config.from_email}>!
+    %Q!"#{encode('つながり短冊')}" <#{Tanzaku::Application.config.from_email}>!
   end
 
   def body(user, target_user)
     <<-EOF
-    こんにちは。つながり短冊からのお知らせです。
+    こんにちは。#{name(user)}さんがつながり短冊に願い事をしました。
 
-    #{user["last_name"]} #{user["first_name"]}さんが#{target_user["last_name"]} #{target_user["first_name"]}さんと会いたいようです。
-    同じイベントに参加する際にはぜひ紹介してあげましょう。
+    ┏━━━━━━━━━━━━━━━━━━━━━━┓
+    　#{name(target_user)}さんに会いたい
+    ┗━━━━━━━━━━━━━━━━━━━━━━┛
+
+    あなたは#{name(user)}さんと#{name(target_user)}さん、どちらも知っていますよね？
+    同じイベント/同じ場所にいる際にはぜひ紹介してあげましょう。
+
+    あなたの協力で、2人はオンラインのつながりからオフラインのつながりに。
 
     ---
     つながり短冊
@@ -33,5 +39,9 @@ module Mailer
 
   def encode(subject)
     NKF.nkf('-j -W --cp932', subject)
+  end
+
+  def name(obj)
+    "#{obj['last_name']} #{obj['first_name']}"
   end
 end
